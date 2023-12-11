c7cadec2967a497b4cfa0345d9476efd7199d4ea
pausme <zx2086709099@163.com>
2023-12-11 19:49:37 +0800

    t-distribution函数实现

--- DBO/其他论文改进点/测试函数/T-distribution.py
+++ DBO/其他论文改进点/测试函数/T-distribution.py
@@ -1,8 +1,10 @@
 # t-distribution
 from scipy.special import gamma
 import math
+import scipy.stats as stats
 
 
+# 自己根据定义所写的方法
 def P(x, m):
     result = (gamma((m + 1) / 2)) / (math.sqrt(m * math.pi) * 
                                      gamma(m / 2)) * math.pow((1 + x * x / 2), -((m + 1) / 2))
@@ -11,5 +13,11 @@ def P(x, m):
 # 当t(m→∞)→N(0,1),t(m→1)=C(0,1), 
 # N(0,1)is Gaussian distribution and C (0,1) is Cauchy distribution.
 
+# 使用scipy中t分布方法
+def T(df, x):
+    # df 为自由度 ， x为输入值
+    return stats.t.pdf(x, df)
+
 result = P(0, 20)
-print(result)
+result1 = T(20, 0)
+print(result, result1)
