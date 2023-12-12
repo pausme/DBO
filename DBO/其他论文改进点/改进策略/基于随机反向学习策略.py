705952669fd3e41dc033917a3b69e36344b20fce
pausme <zx2086709099@163.com>
2023-12-12 15:00:43 +0800

    基于随机反向学习策略

--- DBO/其他论文改进点/改进策略/基于随机反向学习策略.py
+++ DBO/其他论文改进点/改进策略/基于随机反向学习策略.py
@@ -7,3 +7,12 @@ def initialize(x, t, t_max, lb, ub):
     if np.random.random() < t / t_max:
         x = ub + lb - np.random.random() * x
     return x
+
+x = 5
+t = 10
+t_max = 100
+lb = 0
+ub = 10
+
+result = initialize(x, t, t_max, lb, ub)
+print(result)
